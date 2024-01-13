a20c1a52b52a1fe263e20cc58c35938240b86511
LAPTOP-G27QSL1A\Julian <bobyking3333@gmail.com>
2024-01-13 12:36:51 +0100

    ShoppingCart implementiert

--- src/at/wifi/codingacademy/shoppingcart/Program.java
+++ src/at/wifi/codingacademy/shoppingcart/Program.java
@@ -1,13 +1,15 @@
 package at.wifi.codingacademy.shoppingcart;
 
-import at.wifi.codingacademy.shoppingcart.model.ElectronicDevice;
-import at.wifi.codingacademy.shoppingcart.model.Food;
-import at.wifi.codingacademy.shoppingcart.model.FoodCategory;
+import at.wifi.codingacademy.shoppingcart.model.*;
 
 import java.util.Date;
 
+
 public class Program {
     public static void main(String[] args){
+        ShoppingCart sc =new ShoppingCart();
+
+
         ElectronicDevice tv = new ElectronicDevice("SAM55X", 999.99,"Samsung X-Series 55 zoll",
                 10, 100, false);
         ElectronicDevice pixel = new ElectronicDevice("PIX8PRO", 1200,"Google Pixel Z-Series",
@@ -17,12 +19,18 @@ public class Program {
         Food pizza = new Food("PZ3231", 9.99,"Provenziale",
                 100, new Date(2025,5,1), 700);
 
+        sc.addItem(new ShoppingCartItem(tv, 1));
+        sc.addItem(new ShoppingCartItem(pixel, 2));
+        sc.addItem(new ShoppingCartItem(apple, 5));
 
         System.out.println(tv);
         System.out.println(pixel);
         System.out.println(apple);
         System.out.println(pizza);
 
+        System.out.println(sc);
+        System.out.println("Anzahl an Produkten: " + sc.getNumberOfCartItems());
+        System.out.println("Gesamtpreis: " + sc.getTotalPrice());
     }
 }
 
